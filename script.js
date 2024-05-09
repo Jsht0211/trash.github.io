b956c9af5033e3538fa39f0e286d8a99aba63887
Jsht0211 <119144030+Jsht0211@users.noreply.github.com>
2024-05-09 18:44:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var urlParams = new URLSearchParams(window.location.search);
 
 document.querySelector("#name").textContent = "Your name is:" + urlParams.get("name");
-let psw = document.querySelector("#psw")
+let psw = document.querySelector("#psw");
 psw.textContent = "Your password is:" + urlParams.get("psw");
 psw.style.color = "red";
+
+let pt = document.querySelector("#pt");
+pt.src = urlParams.get("pt");
